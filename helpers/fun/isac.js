17896bb43f133dcbb4880f5395168b0897ccbc5d
RageOfFire <buihongson453@gmail.com>
2024-02-01 21:02:35 +0700

    add new fun command that all

---
+++ helpers/fun/isac.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "isac",
+    category: "fun",
+    aliases: [
+        "trung "
+    ],
+    run: async ({ client, message }) => {
+        message.react("🇩");
+        message.react("🇲");
+        message.react("🇮");
+        message.react("🇸");
+        message.react("🇦");
+        message.react("🇨");
+    }
+}
